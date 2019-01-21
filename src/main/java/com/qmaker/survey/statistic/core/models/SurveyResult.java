9ec1a04be4d3dbc035137ff3709a50867d7530ef
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2019-01-21 12:55:10 +0000

    one more step

--- src/main/java/com/qmaker/survey/statistic/core/models/SurveyResult.java
+++ src/main/java/com/qmaker/survey/statistic/core/models/SurveyResult.java
@@ -1,20 +1,111 @@
 package com.qmaker.survey.statistic.core.models;
 
+import com.qmaker.core.entities.Author;
 import com.qmaker.core.entities.CopySheet;
 
 public class SurveyResult {
     //TODO voire dans quel mesure le result doit contenir le component supporté qui a joué ce questionnaire.
-    public String id, campaingId, copySheetId, authorId, authorFullName, readerName;
+    public String id, campaignId, copySheetId, authorId, authorDisplayName, readerName;
     public int composedQuestionCount, consultedQuestionCount, totalQuestionCount,
-            successCount, maxSucessCount, faildCount, score, marks, maxMarks, marksAddedCount, marksSubstractedCount;
-    public long timePast, totalAllowedTime;
+            successCount, maxSuccessCount, failedCount, score, marks, maxMarks, marksAddedCount, marksSubtractedCount;
+    public long timeLeft, totalAllowedTime;
     CopySheet copySheet;
 
     public boolean isTimeOut() {
-        return timePast >= totalAllowedTime;
+        return timeLeft >= totalAllowedTime;
     }
 
-    public SurveyResult from(CopySheet copySheet) {
-        return null;
+    public String getId() {
+        return id;
+    }
+
+    public String getCampaignId() {
+        return campaignId;
+    }
+
+    public String getCopySheetId() {
+        return copySheetId;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public String getAuthorDisplayName() {
+        return authorDisplayName;
+    }
+
+    public String getReaderName() {
+        return readerName;
+    }
+
+    public int getComposedQuestionCount() {
+        return composedQuestionCount;
+    }
+
+    public int getConsultedQuestionCount() {
+        return consultedQuestionCount;
+    }
+
+    public int getTotalQuestionCount() {
+        return totalQuestionCount;
+    }
+
+    public int getSuccessCount() {
+        return successCount;
+    }
+
+    public int getMaxSuccessCount() {
+        return maxSuccessCount;
+    }
+
+    public int getFailedCount() {
+        return failedCount;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public int getMaxMarks() {
+        return maxMarks;
+    }
+
+    public int getMarksAddedCount() {
+        return marksAddedCount;
+    }
+
+    public int getMarksSubtractedCount() {
+        return marksSubtractedCount;
+    }
+
+    public long getTimeLeft() {
+        return timeLeft;
+    }
+
+    public long getTotalAllowedTime() {
+        return totalAllowedTime;
+    }
+
+    public CopySheet getCopySheet() {
+        return copySheet;
+    }
+
+    public static SurveyResult from(String campaignId, CopySheet copySheet) {
+        SurveyResult result = new SurveyResult();
+        result.id = copySheet.getId();//TODO revoir quel est le meilleur ID a donne ra cet entité.
+        result.campaignId = campaignId;
+        result.copySheetId = copySheet.getId();
+        Author author = copySheet.getAuthor();
+        if (author != null) {
+            result.authorId = author.getId();
+            result.authorDisplayName = author.getDisplayName();
+        }
+        result.copySheet = copySheet;
+        return result;
     }
 }
--- src/main/java/com/qmaker/survey/statistic/core/models/SurveySheetResult.java
+++ src/main/java/com/qmaker/survey/statistic/core/models/SurveySheetResult.java
@@ -1,8 +1,11 @@
 package com.qmaker.survey.statistic.core.models;
 
 import com.qmaker.core.entities.CopySheet;
+import com.qmaker.core.entities.Qcm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class SurveySheetResult {
     //TODO doit t'il contenir aussi le nm du reader et le coosant utilisé pour le jouer?
@@ -10,14 +13,26 @@ public class SurveySheetResult {
     public boolean composedState, consultedState;
     public int propsitionCount, answerMaxSuccessCount, answerSuccessCount,
             answserFaiedCount, score, marks, maxMars, marksAddedCount,
-            marksSubstractedCount;
+            marksSubtractedCount;
     public long timePast, totalTimeAllowed;
 
     HashMap<String, Boolean> selectedAnswerState = new HashMap<>();
     HashMap<String, Boolean> successAnswerState = new HashMap<>();
 
-    public SurveyResult from(CopySheet copySheet) {
+    public static SurveySheetResult from(Qcm qcm) {
         return null;
     }
 
+    public static List<SurveySheetResult> list(CopySheet copySheet) {
+        List<SurveySheetResult> results = new ArrayList<>();
+        List<Qcm> sheets = copySheet.getSheets();
+        SurveySheetResult result;
+        for (Qcm sheet : sheets) {
+            result = from(sheet);
+            if (result != null) {
+                results.add(result);
+            }
+        }
+        return results;
+    }
 }
