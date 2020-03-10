97ab85121470bc71b16e92c0ee8dfef3b8cb2177
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2020-03-10 19:45:23 +0000

    des modificaton due a androidx

--- src/androidTest/java/com/qmaker/survey/admin/core/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/qmaker/survey/admin/core/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package com.qmaker.survey.admin.core;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.qmaker.survey.statistic.core.test", appContext.getPackageName());
     }
